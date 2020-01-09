1fead392c45dc5153f56ac1d3cc49d5961164436
John19780103 <39822377+John19780103@users.noreply.github.com>
2020-01-09 17:43:42 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,17 +1,13 @@
 from __future__ import unicode_literals
 from flask import Flask, request, abort
 
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
 from linebot.models import *
 
 
 app = Flask(__name__)
-line_bot_api=LineBotApi('yc+KPHXOCoHuJSqYXTKs7kSoaipzx8MgBnbuB9Koh/UFnU8j6vddEXsqftChasIhYzWiKtIyNTGgs1gFwSQnoge6TUWfHyLEPZt1hP4cWapA6+IR50zN3/7og+rJ5MhmH3USfB6yIpI1o3pb43hS0QdB04t89/1O/w1cDnyilFU=')
+line_bot_api=LineBotApi('Hw+z4rxRO0Gp6BzOsELuUkpgaZrAs0CS+I9eJtY4/TRNhvw4c97D6JH77kchqLg0YzWiKtIyNTGgs1gFwSQnoge6TUWfHyLEPZt1hP4cWarA0LKPE8Ix03VUexECZimJPPmgHPaFV+FOB0Oxr8qYwQdB04t89/1O/w1cDnyilFU=')
 handler=WebhookHandler('5aa5e458031f583b300e25e1164906d8')
 line_bot_api.push_message('Ud7ccd071b5025d5a1d5006f4e620545f', TextSendMessage(text='你可以開始了'))
 
