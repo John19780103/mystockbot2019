8aa4f1ed899d4d91b329afbc1b3720f3de2340e2
John19780103 <39822377+John19780103@users.noreply.github.com>
2020-02-05 16:55:19 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def handle_message(event):
             stock_rt['info']['name'],
             stock_rt['info']['code'],
             my_time)
-        content+='-----\n'
+        content += '-----'
         content+='最近五日價格: \n'
             price5 = stock.price[-5:][::-1]
             date5 = stock.date[-5:][::-1]
