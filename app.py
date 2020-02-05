8961aada9042c74b816ee2f3798515da3b2a70f6
John19780103 <39822377+John19780103@users.noreply.github.com>
2020-02-05 17:19:01 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -41,11 +41,11 @@ def handle_message(event):
 	    stock_rt=twstock.realtime.get(text)
 	    my_datetime = datetime.fromtimestamp(stock_rt['timestamp']+8*60*60)
 	    my_time = my_datetime.strftime('%H:%M:%S')
-	    #content += '%s (%s) %s\n' %(
-        #    stock_rt['info']['name'],
-        #    stock_rt['info']['code'],
-        #    my_time)
-        content += '----'
+	    content += '%s (%s) %s\n' %(
+            stock_rt['info']['name'],
+            stock_rt['info']['code'],
+            my_time)
+        content+="===="
         content+='最近五日價格: \n'
             price5 = stock.price[-5:][::-1]
             date5 = stock.date[-5:][::-1]
