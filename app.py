d1a3600c6a7ac145c5a6a2d83c502c341f0a999e
John19780103 <39822377+John19780103@users.noreply.github.com>
2020-02-05 17:47:14 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import *
+import twstock
 
 
 app = Flask(__name__)
