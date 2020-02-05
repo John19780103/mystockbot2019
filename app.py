6c8728cf8e157dd27cc5089c0e46ea2b2e62c5ad
John19780103 <39822377+John19780103@users.noreply.github.com>
2020-02-05 17:42:50 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -48,15 +48,15 @@ def handle_message(event):
 	        my_time)
 	    content+='===='
 	    content+='最近五日價格: \n'
-	        price5 = stock.price[-5:][::-1]
-	        date5 = stock.date[-5:][::-1]
-	        for i in range(len(price5)):
-	            #content += '[%s] %s\n' %(date5[i].strftime("%Y-%m-%d %H:%M:%S"), price5[i])
-	            content += '[%s] %s\n' %(date5[i].strftime("%Y-%m-%d"), price5[i])
-	            line_bot_api_8.reply_message(
-	            event.reply_token,
-	            TextSendMessage(text=content)
-	            )
+	    price5 = stock.price[-5:][::-1]
+	    date5 = stock.date[-5:][::-1]
+	    for i in range(len(price5)):
+	        #content += '[%s] %s\n' %(date5[i].strftime("%Y-%m-%d %H:%M:%S"), price5[i])
+	        content += '[%s] %s\n' %(date5[i].strftime("%Y-%m-%d"), price5[i])
+	        line_bot_api_8.reply_message(
+	        event.reply_token,
+	        TextSendMessage(text=content)
+	        )
 	else:
 	    reply_text = text
 	#message=TextSendMessage(text=event.message.text)
