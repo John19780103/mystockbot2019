db20f1d577700e0b7bad5611e81e71c4d5ae3f1f
John19780103 <39822377+John19780103@users.noreply.github.com>
2020-02-05 15:40:21 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def handle_message(event):
 	else:
 	    reply_text = text
 	#message=TextSendMessage(text=event.message.text)
-	message=TextSendMessage(text)
+	message=TextSendMessage(reply_text)
     line_bot_api.reply_message(event.reply_token,message)
 
 import os
